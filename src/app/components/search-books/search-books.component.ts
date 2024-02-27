ac07654ed76bdcba51bb0c5b7cd9f4ce63e07b9d
HernanRomanello <143692103+HernanRomanello@users.noreply.github.com>
2024-02-27 18:09:33 +0200

    update

--- src/app/components/search-books/search-books.component.ts
+++ src/app/components/search-books/search-books.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BooksService } from '../../services/books/books.service';
 import { AuthService } from '../../services/auth.service';
 import { book } from '../../shared/models/book';
+import { last } from 'rxjs';
 
 @Component({
   selector: 'app-search-books',
@@ -14,21 +15,51 @@ export class SearchBooksComponent implements OnInit {
   minPrice: number = 0;
   maxPrice: number = 0;
   books: book[] = []; // This should be an array of book objects
+  booksFirstIndex: number = 0;
+  booksLastIndex: number = 12;
+  prevButtonActive: boolean = false;
+  nextButtonActive: boolean = true;
 
   constructor(
     private booksservice: BooksService,
     private authService: AuthService
   ) {}
 
+  hideButtons() {
+    if (this.booksFirstIndex === 0) {
+      this.prevButtonActive = false;
+    } else {
+      this.prevButtonActive = true;
+    }
+
+    if (this.booksLastIndex >= this.books.length) {
+      this.nextButtonActive = false;
+    } else {
+      this.nextButtonActive = true;
+    }
+  }
+
   ngOnInit(): void {
     // this.addBooks(); // Call the function to add books when the component initializes
     this.books = this.booksservice.getAllBooks();
+    this.hideButtons();
   }
 
   searchBooks() {
     // Implement search functionality here
     // You can filter books based on the search query, selected filter, minPrice, and maxPrice
     // Update the this.books array with the filtered results
+  }
+
+  prevPage() {
+    this.booksFirstIndex -= 12;
+    this.booksLastIndex -= 12;
+    this.hideButtons();
+  }
+  nextPage() {
+    this.booksFirstIndex += 12;
+    this.booksLastIndex += 12;
+    this.hideButtons();
   }
 
   addBooks() {
