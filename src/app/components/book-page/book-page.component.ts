e0b24788fa5e91c9de30035eb856d6b09b9ccec5
HernanRomanello <143692103+HernanRomanello@users.noreply.github.com>
2024-02-28 17:09:13 +0200

    update

--- src/app/components/book-page/book-page.component.ts
+++ src/app/components/book-page/book-page.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { book } from '../../shared/models/book';
 
 @Component({
   selector: 'app-book-page',
   templateUrl: './book-page.component.html',
-  styleUrl: './book-page.component.css'
+  styleUrl: './book-page.component.css',
 })
-export class BookPageComponent {
+export class BookPageComponent implements OnInit {
+  book!: book;
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
