1060cc83b83bd5d93ef451548b44ea232595a7ce
HernanRomanello <143692103+HernanRomanello@users.noreply.github.com>
2024-03-03 17:42:19 +0200

    update

--- src/app/components/page-404/page-404.component.ts
+++ src/app/components/page-404/page-404.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-page-404',
   templateUrl: './page-404.component.html',
-  styleUrl: './page-404.component.css'
+  styleUrl: './page-404.component.css',
 })
 export class Page404Component {
-
+  _404Pic = 'assets/images/404/404.jpg';
 }
